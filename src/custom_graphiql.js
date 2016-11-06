06e1711535506159da3944ba01159c7acbb03cc3
fanzhenchao <fanzhenchao@growingio.com>
2016-11-07 00:22:30 +0800

    first commit

---
+++ src/custom_graphiql.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import fetch from 'isomorphic-fetch';
+import GraphiQL from 'graphiql';
+
+class CustomGraphiQL extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      url: props.url || (window.location.origin + '/graphql'),
+      inputVisible: false
+    };
+
+    this.fetcher = this.fetcher.bind(this);
+    this.handleUrlChange = this.handleUrlChange.bind(this);
+    this.showUrlInput = this.showUrlInput.bind(this);
+    this.saveUrl = this.saveUrl.bind(this);
+  }
+
+  fetcher(graphQLParams) {
+    return fetch(this.state.url, {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(graphQLParams)
+    }).then(response => response.json());
+  }
+
+  handleUrlChange(event) {
+    this.setState({ url: event.target.value });
+  }
+
+  showUrlInput() {
+    this.setState({ inputVisible: true });
+  }
+
+  saveUrl(event) {
+    if (event.key === 'Enter') {
+      this.setState({ inputVisible: false });
+      // introspection requests
+      this.refs.graphiql._ensureOfSchema();
+    }
+  }
+
+  render() {
+    const { props } = this;
+    return (
+      <GraphiQL {...props} fetcher={this.fetcher} ref='graphiql'>
+        <GraphiQL.Toolbar>
+          { this.state.inputVisible && (
+            <input
+              className='url-input'
+              type='text'
+              onKeyPress={this.saveUrl}
+              onChange={this.handleUrlChange}
+              value={this.state.url}
+            />
+          )}
+          { !this.state.inputVisible && <span className='url-span' onClick={this.showUrlInput}>{this.state.url}</span> }
+        </GraphiQL.Toolbar>
+      </GraphiQL>
+    );
+  }
+}
+
+CustomGraphiQL.propTypes = {};
+
+export default CustomGraphiQL;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CustomGraphiQL from './custom_graphiql';
+import '../node_modules/graphiql/graphiql.css';
+import './index.css';
+
+ReactDOM.render(
+  <CustomGraphiQL />,
+  document.body
+);
